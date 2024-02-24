460169b072e3f291056b5fa52f9f7c2b6ae36fef
Edgidio <edgidio.leon@gmail.com>
2024-02-23 20:57:55 -0400

    Ruta

--- src/index.js
+++ src/index.js
@@ -3,6 +3,6 @@ import server from "./app.js"
 const port = process.env.PORT;
 const host = process.env.HOST ;
 
-server.listen(port/* , "0.0.0.0" */, () => {
+server.listen(port, host, () => {
   console.log(`Servidor corriendo en el puerto: ${port}`)
 })
